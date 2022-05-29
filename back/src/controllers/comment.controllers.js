edb7c9f825c683b4b6c900c3ae0f2d4f65b443a9
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-29 07:40:29 -0500

    fix: rename controllers

---
+++ back/src/controllers/comment.controllers.js
@@ -0,0 +1,42 @@
+const createError = require("http-errors");
+const ImgSchema = require("../models/img.schema");
+const CommentSchema = require("../models/comment.schema");
+
+class CommentController {
+  async getComentByImg (req, res) {
+    res.json("get coment");
+  };
+
+  async getAllComent (req, res) {
+  // await ImgSchema.findById(req.params.id).populate("comments");
+    const comment = await CommentSchema.find({ imgId: req.params.id }).populate("userId", { password: 0 }).sort({ createdAt: -1 });
+    res.json(comment);
+  };
+
+  async addComment (req, res, next) {
+    const img = await ImgSchema.findById(req.body.imgId);
+    if (!img) return next(createError(404, "Img not found"));
+
+    const comment = new CommentSchema({
+      comment: req.body.comment,
+      imgId: req.body.imgId,
+      userId: req.user._id
+    });
+    const comentSave = await comment.save();
+    // get comment with user
+    const comentPopulate = await CommentSchema.findById(comentSave._id).populate("userId", { password: 0 });
+    img.comments.push(comment._id);
+    await img.save();
+    res.json(comentPopulate);
+  };
+
+  async updateComentById (req, res) {
+    res.json("update coment");
+  };
+
+  async deleteComent (req, res, next) {
+    res.json("delete coment");
+  };
+}
+
+module.exports = new CommentController();
---
+++ back/src/controllers/img.controllers.js
@@ -0,0 +1,99 @@
+
+const fs = require("fs");
+const createError = require("http-errors");
+
+const { cloudinaryAdd, cloudinaryDelete } = require("../helper/cloudinary");
+
+// Schema Image
+const ImgSchema = require("../models/img.schema");
+
+class ImgController {
+  async getImgById (req, res, next) {
+    const id = req.params.id;
+
+    try {
+      const img = await ImgSchema.findById(id);
+
+      if (!img) throw createError.BadRequest("image not found");
+
+      return res.json(img);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  // get img by id user
+  async getImgs (req, res, next) {
+    try {
+      const imgs = await ImgSchema.find({ userId: req.user.id });
+      if (!imgs) throw createError.badRequest("not found image");
+      res.json(imgs);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  // get all img
+  async getAllImg (req, res, next) {
+    try {
+      const img = await ImgSchema.find().populate("userId", { password: 0 });
+      if (!img) throw createError.BadRequest("not found images");
+
+      return res.json(img);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  async uploadImg (req, res, next) {
+    const img = await new ImgSchema();
+
+    try {
+      if (!req.files) throw createError.BadRequest();
+      if (!req.body.title || !req.body.description) { throw createError.Unauthorized("you have complete form"); }
+      const imgCloud = await cloudinaryAdd(req.files.image.tempFilePath);
+      if (!imgCloud) throw createError.Unauthorized("not provided image");
+      img.title = req.body.title;
+      img.description = req.body.description;
+      img.userId = req.user.id;
+      img.imgUrl = imgCloud.url;
+      const imgSave = await img.save();
+      await fs.unlink(req.files.image.tempFilePath, () => console.log("deleted"));
+      res.json(imgSave);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  async deleteImg (req, res, next) {
+    const id = req.params.id;
+    try {
+      const img = await ImgSchema.findByIdAndRemove(id);
+      if (!img) throw createError.BadRequest("image not found");
+      // cut url
+      const imgCut = img.imgUrl.split("/")[7].split(".")[0];
+      // delete img cloudinary
+      await cloudinaryDelete(imgCut);
+
+      return res.json(img);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  async updateImg (req, res, next) {
+    const id = req.params.id;
+    console.log(req.body);
+    try {
+      const img = await ImgSchema.findByIdAndUpdate(id, req.body);
+      if (!img) throw createError.BadRequest("image not found");
+
+      return res.json(img);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+;
+
+module.exports = new ImgController();
