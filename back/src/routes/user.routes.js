e6ce545abd193a382fc7391537133ab64d71e0ab
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-06-05 17:20:15 -0500

    feat: user routes

--- back/src/routes/user.routes.js
+++ back/src/routes/user.routes.js
@@ -3,5 +3,9 @@ const router = Router();
 const UserController = require("../controllers/user.controller");
 
 router.get("/", UserController.getAllUser);
+router.get("/:id", UserController.getUserById);
+router.get("/email/:email", UserController.getUserByEmail);
+router.put("/update/:id", UserController.updateUser);
+router.delete("/delete/:id", UserController.deleteUser);
 
 module.exports = router;
