c1cdf1239f0555d81f3192aabfddf26306ccd635
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-06-19 18:13:38 -0500

    feat: service note

---
+++ back/src/services/note.service.js
@@ -0,0 +1,44 @@
+const Note = require("../models/note.schema");
+class NoteService {
+  async findAllNotes () {
+    try {
+      return await Note.find();
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async findNoteById (id) {
+    try {
+      return await Note.findById(id);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async createNote (data) {
+    try {
+      return await new Note(data);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async deleteNoteById (id) {
+    try {
+      return await Note.findByIdAndDelete(id);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  async updateNoteById (id, data) {
+    try {
+      return await Note.findByIdAndUpdate(id, data);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
+
+module.exports = new NoteService();
