c87792bdb745f9f9de26978907c41d408d53ceeb
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-07-29 10:25:02 -0500

    feat: upgrade connection

--- back/src/service.js
+++ back/src/service.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
-const API_URI = ` mongodb+srv://joeluser:${process.env.PASSWORD_DB}@cluster0.m8z6c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const API_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.m8z6c.mongodb.net/${process.env.MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 
 mongoose.connect(API_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
-}).then(() => console.log("conect db"));
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() => console.log("conect db to mongodb"))
+  .catch(err => console.log(err));
 
 module.exports = mongoose;
