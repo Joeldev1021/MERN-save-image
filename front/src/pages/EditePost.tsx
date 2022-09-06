6df306460d6eab81ff03a6d1284302ad9dd60c7f
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-09-06 17:47:25 -0500

    feat: page edite Post and myposts

---
+++ front/src/pages/EditePost.tsx
@@ -0,0 +1,91 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Input from '../components/Input';
+import { PostContext } from '../context/post-image/PostContext';
+import { IPostUser } from '../interface';
+
+function EditePost() {
+	const { id } = useParams();
+	const { posts } = useContext(PostContext);
+	const [editePost, setEditePost] = useState<IPostUser>();
+
+	useEffect(() => {
+		const postFound = posts.find(post => post._id === id);
+		setEditePost(postFound);
+	}, [id]);
+
+	const handleChange = () => {};
+	const handleSubmit = () => {};
+
+	return (
+		<div className="relative  flex items-center justify-center sm:px-6 lg:px-8  bg-no-repeat">
+			<div className="absolute inset-0 z-0"></div>
+			<div className="sm:max-w-lg w-full p-5 bg-white rounded-xl z-10">
+				<div className="text-center">
+					<h2 className="mt-5 text-3xl font-bold text-gray-900">Edite Post!</h2>
+				</div>
+				{editePost && (
+					<form className="mt-8 space-y-3" onSubmit={handleSubmit}>
+						<div className="grid grid-cols-1 space-y-2">
+							<label className="text-sm font-bold text-gray-500 tracking-wide">
+								Title
+							</label>
+							<Input
+								type="text"
+								placeholder="Title"
+								defaultValue={editePost.title}
+								name="title"
+								handleChange={handleChange}
+							/>
+						</div>
+						<div className="grid grid-cols-1 space-y-2">
+							<label className="text-sm font-bold text-gray-500 tracking-wide">
+								Description
+							</label>
+							<Input
+								type="text"
+								placeholder="Description"
+								defaultValue={editePost.description}
+								name="description"
+								handleChange={handleChange}
+							/>
+						</div>
+						<div className="grid grid-cols-1 space-y-2">
+							<label className="text-sm font-bold text-gray-500 tracking-wide">
+								Image
+							</label>
+							<div className="flex items-center justify-center w-full">
+								<label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
+									<div className="h-full w-full text-center flex flex-col items-center justify-center   ">
+										<div className="flex flex-auto max-h-48 w-2/5 mx-auto -mt-10">
+											<img
+												className="has-mask h-36 object-contain"
+												src={editePost?.imgUrl}
+												alt="freepik image"
+											/>
+										</div>
+									</div>
+									<input type="file" name="file" className="hidden" />
+								</label>
+							</div>
+						</div>
+						<p className="text-sm text-gray-300">
+							<span>File type: doc,pdf,types of images</span>
+						</p>
+						<div>
+							<button
+								type="submit"
+								className="my-5 w-full flex justify-center bg-blue-500 text-gray-100 p-4  rounded-full tracking-wide
+                                    font-semibold  focus:outline-none focus:shadow-outline hover:bg-blue-600 shadow-lg cursor-pointer transition ease-in duration-300"
+							>
+								Update
+							</button>
+						</div>
+					</form>
+				)}
+			</div>
+		</div>
+	);
+}
+
+export default EditePost;
---
+++ front/src/pages/MyPost.tsx
@@ -0,0 +1,37 @@
+import React, { useContext, useEffect } from 'react';
+import { AuthContext } from '../context/auth/AuthContext';
+import { PostContext } from '../context/post-image/PostContext';
+import CardDesing from '../components/CardDesing';
+
+const MyPost = () => {
+	const { state } = useContext(AuthContext);
+	const { posts, getPost } = useContext(PostContext);
+	useEffect(() => {
+		getPost();
+	}, []);
+	return (
+		<div>
+			<h1>my post</h1>
+			{state.token ? (
+				posts.map(post => {
+					return (
+						<CardDesing
+							key={post._id}
+							id={post._id}
+							title={post.title}
+							desc={post.description}
+							img={post.imgUrl}
+							comments={post.comments}
+							likes={post.likes}
+							createdAt={post.created_at}
+						/>
+					);
+				})
+			) : (
+				<h1>no login</h1>
+			)}
+		</div>
+	);
+};
+
+export default MyPost;
