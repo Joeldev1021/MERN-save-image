a7cdc2018bf95924076151698a9334c3d7442d81
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-09-07 08:27:57 -0500

    fix: update routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,12 +2,27 @@ import CardList from './components/CardList';
 import Header from './components/Header/Header';
 import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/auth/LoginPage';
+import Notes from './pages/NotePage';
+import MyPost from './pages/MyPost';
+import RegisterPage from './pages/auth/RegisterPage';
+import Upload from './pages/Upload';
+import ProfilePost from './pages/ProfilePost';
+import Settings from './pages/Settings';
+import EditePost from './pages/EditePost';
+
 function App() {
 	return (
 		<>
 			<Header />
 			<Routes>
 				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
+				<Route path="/notes" element={<Notes />} />
+				<Route path="/my-post" element={<MyPost />} />
+				<Route path="/post/:id" element={<ProfilePost />} />
+				<Route path="/post/edite/:id" element={<EditePost />} />
+				<Route path="/upload" element={<Upload />} />
+				<Route path="/settings" element={<Settings />} />
 				<Route path="/" element={<CardList />} />
 			</Routes>
 		</>
