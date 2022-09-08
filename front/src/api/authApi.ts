c5180054d6ae43963c4347597e624dde1352bb62
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-09-08 10:12:22 -0500

    feat: get profile by token

--- front/src/api/authApi.ts
+++ front/src/api/authApi.ts
@@ -1,12 +1,14 @@
 import axios from 'axios'
-import { IUserLogin } from "../interface";
+import { IUser, IUserLogin } from "../interface";
 
 
 interface ILoginApi {
     token: string;
 }
 
-export const loginApi = async (user: IUserLogin) => await axios.post<ILoginApi>(`http://localhost:5000/auth/signin`, user);
+
+
+export const loginApi = async (user: IUserLogin) => axios.post<ILoginApi>(`http://localhost:5000/auth/signin`, user);
 
 export const logoutApi = async (token: string) => {
     const response = await fetch('http://localhost:5000/auth/logout', {
@@ -17,4 +19,12 @@ export const logoutApi = async (token: string) => {
         }),
     })
     return response.json()
+}
+
+export const getProfileApi = async (token: string) => {
+    return axios.get<IUser>('http://localhost:5000/user/profile', {
+        headers: {
+            'Authorization': token
+        }
+    })
 }
