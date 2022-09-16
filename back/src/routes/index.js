ddab900fa2607d786a637fda417085cbbf82a1cc
joeldev <joelpimentel1995@gmail.com>
2022-09-16 10:22:10 -0500

    feat: delete note

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 const router = Router();
 
-const noteRoutes = require('./note.routes');
 const imageRoutes = require('./img.routes');
 const authRoutes = require('./auth.routes');
 const userRoutes = require('./user.routes');
@@ -10,7 +9,6 @@ const addLikeRotes = require('./like.routes');
 
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
-router.use('/note', noteRoutes);
 router.use('/img', imageRoutes);
 router.use('/img-comment', commentRoutes);
 router.use('/img-like', addLikeRotes);
